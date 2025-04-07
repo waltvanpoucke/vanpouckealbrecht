8b352fa313e098359cdbd8f892f064a1bf3bf18c
waltvanpoucke <113846169+waltvanpoucke@users.noreply.github.com>
2025-04-07 16:50:58 +0200

    Add files via upload

---
+++ toon_waarden_post.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+
+	<head>
+		<title>Formulieren</title>
+	</head>
+	
+	<body>
+		
+		
+		<?php
+
+        		
+		// afdrukken van de $_POST array
+		
+        print "<pre>" ;        
+        print_r ($_POST) ;        
+        print "</pre>" ;            
+        
+        ?>
+		
+	   	
+	</body>	
+	
+</html>
